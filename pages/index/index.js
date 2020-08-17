e2a8c72c8a82dea072a976b440b6dff06540e512
Rave <46985889+liangguangtou@users.noreply.github.com>
2020-08-17 11:50:31 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,11 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    imgUrls: [
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597228673440&di=7023910f9490bbab5278988a4f5b0a43&imgtype=0&src=http%3A%2F%2Fpic-bucket.ws.126.net%2Fphoto%2F0005%2F2020-08-10%2FFJLIL9U24TM10005NOS.jpg%3FimageView%26amp%3Bthumbnail%3D140y98',
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597228807820&di=9b21c6c69fe4585d0f2b825cade73117&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsports%2F2_img%2Fdfic%2Fcf0d0fdd%2F107%2Fw1024h683%2F20200812%2F2d36-ixreehn9892912.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597228807819&di=791c5c1ac469b163fe927850de0db475&imgtype=0&src=http%3A%2F%2F03imgmini.eastday.com%2Fmobile%2F20181215%2F20181215114441_8d3e4eaced96b57af4a0cdc5979cb75e_3.jpeg']
+  },
+})
---
+++ pages/login/login.js
@@ -0,0 +1,80 @@
+// pages/login/login.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+        username:null,
+        password:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  loginBtnClick: function(){
+          app.appData.userinfo = {username:this.data.username, password:this.data.password}
+          wx.redirectTo({url:"../user/user"})     
+  },
+
+  usernameInput: function(event){
+        this.setData({username:event.detail.value})
+  },
+
+  passwordInput: function(event){
+      this.setData({password:event.detail.value})   
+  }
+})
